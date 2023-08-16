4954f000c1c6f60f9180f1e41e8f2b2b13326aac
kibroman <you@example.comkibromanx@gmail.com>
2023-08-15 22:43:22 -0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,106 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useState, useEffect } from 'react';
+// import lakitu from './../assets/lakitu.png'
+
+import styles from "./page.module.css";
 
 export default function Home() {
+
+  const lakitu = require('../assets/lakitu.png');
+
+  const initialCount = 3;
+  const [redActive, setRedActive] = useState(false);
+  const [yellowActive, setYellowActive] = useState(false);
+  const [greenActive, setGreenActive] = useState(false);
+  const [count, setCount] = useState(initialCount);
+  const [raceStart, setRaceStart] = useState(false);
+
+  const lightColor = {
+    redLight: {
+      backgroundColor: redActive ? "red" : "",
+      
+    },
+    yellowLight: {
+      backgroundColor: yellowActive ? "yellow" : "",
+      
+    },
+    greenLight: {
+      backgroundColor: greenActive ? "green" : "",
+      
+    },
+  };
+
+  const resetCount = () => {
+    setCount(initialCount);
+  };
+
+  const activateRed = () => {
+    setRedActive((prevState) => !prevState);
+    setYellowActive(false);
+    setGreenActive(false);
+  };
+  const activateYellow = () => {
+    setYellowActive((prevState) => !prevState);
+    setRedActive(false);
+    setGreenActive(false);
+  };
+  const activateGreen = () => {
+    setGreenActive((prevState) => !prevState);
+    setYellowActive(false);
+    setRedActive(false);
+  };
+  const starting = () => {
+    console.log('Start!')
+    setRaceStart((prevState) => !prevState);
+  }
+ 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count > 0 && count === 3) {
+        activateRed();
+        setCount(count - 1);
+      }
+      if (count > 0 && count === 2) {
+        activateYellow();
+        setCount(count - 1);
+      }
+      if (count > 0 && count === 1) {
+        activateGreen();
+        setCount(count - 1);
+      }
+      if (count === 0) {
+        starting();
+        resetCount();
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      {/* <img src={lakitu} alt="lakitu" /> */}
+      <div className={styles.lights}>
+        <div
+          style={lightColor.redLight}
+
+          className={styles.redlight}
+        ></div>
+        <div
+          style={lightColor.yellowLight}
+
+          className={styles.yellowlight}
+        ></div>
+        <div
+          style={lightColor.greenLight}
+
+          className={styles.greenlight}
+        ></div>
       </div>
+    </>
+  );
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
 }
